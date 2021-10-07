ce7a29917aac9e3466d4461268205660b2c2d04e
0917863cmgt <0917863@hr.nl>
2021-10-07 13:57:41 +0200

    Changed some user table column names again

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('middle_name')->nullable();
             $table->string('insertion', 20)->nullable();
-            $table->string('last-name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
