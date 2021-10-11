2c96ee77fb9b9082a761ce5457d79ca742878990
0917863cmgt <0917863@hr.nl>
2021-10-11 10:24:32 +0200

    Changed file name

--- app/Models/Article_old.php
+++ app/Models/Article_old.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-class Article
+class Article_old
 {
     public $title;
     public $excerpt;
