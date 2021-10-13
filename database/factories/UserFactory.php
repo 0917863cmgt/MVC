530dd51c97e756cf3dd90b4b0198bd137b697fb2
0917863cmgt <0917863@hr.nl>
2021-10-13 13:56:57 +0200

    Updated UserFactory to the new attributes for the related migration table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Provider\Address;
 
 class UserFactory extends Factory
 {
@@ -23,11 +24,25 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'role' => 1,
+            'first_name' => $this->faker->unique->firstName(),
+            'middle_name' => $this->faker->unique->firstName(),
+            'prefix' => $this->faker->randomElement(['van', 'van der', 'de', 'von', 'de la', 'van de', 'van den', 'den', 'dos', '', '']),
+            'last_name' => $this->faker->unique->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'gender' => array_rand(['Male', 'Female']),
+            'birthdate' => $this->faker->date(),
+            'street_name' => $this->faker->streetName(),
+            'house_number_and_extension' => $this->faker->buildingNumber(),
+            'postal_code' => Address::postcode(),
+            'city' => $this->faker->city,
+            'country' => $this->faker->country(),
+            'phone_number'=> $this->faker->phoneNumber(),
+            'profile_image' => $this->faker->imageUrl,
+            'background_image' => $this->faker->imageUrl,
+            'remember_token' => Str::random(10)
         ];
     }
 
