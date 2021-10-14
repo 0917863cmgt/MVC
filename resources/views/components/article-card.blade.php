1a4007fdc2cc6fc7647f653687a1f470876cf902
0917863cmgt <0917863@hr.nl>
2021-10-14 17:49:58 +0200

    Minor styling related changes

--- resources/views/components/article-card.blade.php
+++ resources/views/components/article-card.blade.php
@@ -1,13 +1,11 @@
-<div class="p-3 m-1 col-3 border">
+<div class="p-3 m-1 col-4 border">
     <img id="featured-image" src="{{$article->image}}">
-    <h5 style="margin-top: 10px; min-height: 72px; max-height: 72px;">{{$article->title}}</h5>
-    <p>{{\Carbon\Carbon::parse($article->publish_date)->format('d/m/Y')}}</p>
-    <a class="category" href="/news/category/{{$article->category->slug}}">{{$article->category->name}}</a>
-    <br>
+    <h5 style="margin-top: 20px; min-height: 72px; max-height: 72px;">{{$article->title}}</h5>
+    <p>{{\Carbon\Carbon::parse($article->publish_date)->format('d/m/Y')}} <a class="category" style="float: right" href="/news/category/{{$article->category->slug}}">{{$article->category->name}}</a></p>
     <p>{{Illuminate\Support\Str::limit($article->summary, 50)}}</p>
     <br>
     <div class="row">
-        <div class="col-6"><p>By {{$article->author->first_name}} {{$article->author->middle_name}} {{$article->author->prefix}} {{$article->author->last_name}}</p></div>
-        <div class="col-6"><a class="read-more" href="/news/{{$article->slug}}">Lees meer</a></div>
+        <div class="col-8"><p>By {{$article->author->first_name}} {{$article->author->middle_name}} {{$article->author->prefix}} {{$article->author->last_name}}</p></div>
+        <div class="col-4"><a class="read-more" href="/news/{{$article->slug}}">Lees meer</a></div>
     </div>
 </div>
