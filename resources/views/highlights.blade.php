3611b13cc12770335b724da8ce192c2db1bf140a
0917863cmgt <0917863@hr.nl>
2021-10-15 17:46:51 +0200

    Better styling for the highlights overview template

--- resources/views/highlights.blade.php
+++ resources/views/highlights.blade.php
@@ -3,7 +3,17 @@
     <body id="highlights">
     <section id="highlights-section" class="container-fluid">
         <div class="row">
-
+            <div class="col-8 offset-2">
+                <div class="row g-3 row-cols-3">
+                    @foreach($highlights as $highlight)
+                        <div class="col">
+                            <a href="/highlights/h/{{$highlight->slug}}">
+                                <img class="highlight-block" src="{{$highlight->media}}">
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </section>
     </body>
