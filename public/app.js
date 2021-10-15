7fcb0b7b380b1ff1dc738858df839f5fcdb4272e
0917863cmgt <0917863@hr.nl>
2021-10-15 17:48:17 +0200

    Added new js file with query selectors for two different carousels

---
+++ public/app.js
@@ -0,0 +1,5 @@
+var myCarousel = document.querySelector('#carouselExampleDark')
+var carousel = new bootstrap.Carousel(myCarousel)
+var myCarousel2 = document.querySelector('#carouselExampleDark2')
+var carousel2 = new bootstrap.Carousel(myCarousel2)
+
