b53f3dba7c8946cbf87ea07f62ea1097b451bfca
0917863cmgt <0917863@hr.nl>
2021-10-28 13:52:52 +0200

    Extracted html code for the articles template into seperate components.

---
+++ resources/views/components/article-div.blade.php
@@ -0,0 +1,15 @@
+@props(['article'])
+<div {{ $attributes->merge(['class' => 'col-6 offset-3']) }}>
+    <article>
+        <p><a class="category" href="/news/category/{{$article->category->slug}}">{{$article->category->name}}</a></p>
+        <h1>
+            <a href="/news/{{$article->slug}}" style="color:rgb(33, 37, 41);text-decoration: none;">
+                {{$article->title}}
+            </a>
+        </h1>
+        <div>
+            <p>By {{$article->author->first_name}} {{$article->author->middle_name}} {{$article->author->prefix}} {{$article->author->last_name}}</p>
+            <p>{{$article->summary}}</p>
+        </div>
+    </article>
+</div>
---
+++ resources/views/components/searchbar.blade.php
@@ -0,0 +1,5 @@
+<div style="max-width: 200px;margin: 0;">
+    <form method="GET" action="#">
+        <input type="text" name="search" placeholder="Search..." class="article-searchbar">
+    </form>
+</div>
