3bc6cd696f7a4b533a1887d8808dd799eca95deb
0917863cmgt <0917863@hr.nl>
2021-10-28 13:54:25 +0200

    Added layout components to the news template

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,26 +1,23 @@
 @extends('layout')
 @section('content')
 <body id="news">
-<section id="news-container" class="container">
+<section id="news-container" class="container-fluid">
+    <x-articles-header></x-articles-header>
     @foreach($articles as $article)
         <div class="row">
-            <div class="col-6 offset-3">
-                <article>
-                    <p><a href="/news/category/{{$article->category->slug}}">{{$article->category->name}}</a></p>
-                    <h1>
-                        <a href="/news/{{$article->slug}}">
-                            {{$article->title}}
-                        </a>
-                    </h1>
-                    <div>
-                        <p>By {{$article->author->first_name}} {{$article->author->middle_name}} {{$article->author->prefix}} {{$article->author->last_name}}</p>
-                        <p>{{$article->summary}}</p>
-                    </div>
-                </article>
-            </div>
+            @if($loop->first)
+            <x-article-div :article="$article"/>
+            @else
+            <x-article-div class="article-separator" :article="$article"/>
+            @endif
         </div>
         {{--        @dd($loop)--}}
     @endforeach
+    @if($articles->count() <1)
+        <div class="col-6 offset-3 no-articles" style="height: calc(40vh - 150px);">
+            <p style="text-align: center;">No articles found</p>
+        </div>
+    @endif
 </section>
 </body>
 @endsection
