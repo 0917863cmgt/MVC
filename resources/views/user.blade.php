319fe5be052bbdfdbd332e9688d0745ec0165f9a
0917863cmgt <0917863@hr.nl>
2021-10-29 22:56:23 +0200

    Added user overview template

---
+++ resources/views/user.blade.php
@@ -0,0 +1,8 @@
+@extends('components.layout')
+@section('content')
+    <body id="user">
+        <section id="user-section" class="container-fluid">
+            {{dd($user)}}
+        </section>
+    </body>
+@endsection
