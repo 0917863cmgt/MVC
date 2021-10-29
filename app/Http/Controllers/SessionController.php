a5856ebb22d2a161ab8f2306f24de7740ab2537f
0917863cmgt <0917863@hr.nl>
2021-10-29 22:51:55 +0200

    Added session controller with working basic log in and log out

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    public function create(){
+        return view('session.create');
+    }
+    public function store(){
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if(auth()->attempt($attributes)){
+            session()->regenerate();
+            return redirect('/')->with('succes', 'U ben succesvol ingelogd.');
+        }
+        else {
+            return back()->withInput()
+                ->withErrors(['email' => 'Wrong email','password' => 'Wrong password']);
+        }
+    }
+    public function destroy(){
+        auth()->logout();
+        return redirect('/')->with('succes', 'U bent succesvol uitgelogd.');
+    }
+}
