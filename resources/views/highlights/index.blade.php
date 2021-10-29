b019bd9dfa8a730c7bcbe3a1fe8928ef4a22f590
0917863cmgt <0917863@hr.nl>
2021-10-29 16:48:59 +0200

    Nothing changed

--- resources/views/highlights/index.blade.php
+++ resources/views/highlights/index.blade.php
@@ -1,20 +1,20 @@
 @extends('components.layout')
 @section('content')
     <body id="highlights">
-    <section id="highlights-section" class="container-fluid">
-        <div class="row">
-            <div class="col-8 offset-2">
-                <div class="row g-3 row-cols-3">
-                    @foreach($highlights as $highlight)
-                        <div class="col">
-                            <a href="/highlights/s/{{$highlight->slug}}">
-                                <img class="highlight-block" src="{{$highlight->media}}">
-                            </a>
-                        </div>
-                    @endforeach
+        <section id="highlights-section" class="container-fluid">
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div class="row g-3 row-cols-3">
+                        @foreach($highlights as $highlight)
+                            <div class="col">
+                                <a href="/highlights/s/{{$highlight->slug}}">
+                                    <img class="highlight-block" src="{{$highlight->media}}">
+                                </a>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     </body>
 @endsection
