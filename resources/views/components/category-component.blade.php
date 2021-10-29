67ab5719876c20bbdf2a6ce39d2cb2748186aea1
0917863cmgt <0917863@hr.nl>
2021-10-29 16:48:27 +0200

    Added tabindex parameters to category dropdown

--- resources/views/components/category-component.blade.php
+++ resources/views/components/category-component.blade.php
@@ -1,9 +1,9 @@
-<nav class="dropdown-select">
-    <ul>
-        <li aria-haspopup="true">{{isset($category) ? $category->name : 'Categories'}}
-            <ul aria-label="submenu">
+<nav class="dropdown-select" tabindex=0>
+    <ul tabindex=0>
+        <li tabindex=0 aria-haspopup="true">{{isset($category) ? $category->name : 'Categories'}}
+            <ul tabindex=0 aria-label="submenu">
                 @foreach($categories as $category)
-                    <li>
+                    <li tabindex=0>
                         <a class="n-t-d" href="/news/?category={{$category->slug}}&{{http_build_query(request()->except('category', 'page'))}}">{{$category->name}}</a>
                     </li>
                 @endforeach
