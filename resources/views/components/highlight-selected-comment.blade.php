8c41aaeb70c4fb15c9d4b06683a9f1eb9431ce06
0917863cmgt <0917863@hr.nl>
2021-10-31 23:31:06 +0100

    Added the functionality for users to add en destroy likes on highlights overview template

--- resources/views/components/highlight-selected-comment.blade.php
+++ resources/views/components/highlight-selected-comment.blade.php
@@ -1,4 +1,4 @@
-@props(['comment', 'selected'])
+@props(['comment', 'selected', 'hasCommentLike', 'commentLike'])
 <ul class="Mr508" style="margin-bottom: 16px; padding:0; border: 0;">
     <div role="button" class="ZyFrc" tabindex="0" style="
     display: flex;
@@ -71,7 +71,38 @@
                 </div>
                 <span class="jdtwu">
                     <div class="_2ic5v">
-                        <form class="like-highlight-form" method="POST" action="/highlights/s/{{$selected->slug}}/like-comment">
+                        @if($hasCommentLike == true)
+                            <form class="like-highlight-form" method="POST" action="/highlights/s/{{$selected->slug}}/destroy-comment-like/{{$commentLike->id}}">
+                                @csrf
+                                <input type="hidden" name="highlight_id" id="highlight_id" value="{{$selected->id}}">
+                                <input type="hidden" name="comment_id" id="comment_id" value="{{$comment->id}}">
+                                <input type="hidden" name="user_id" id="user_id" value="{{auth()->user()->id}}">
+                                <button class="highlight-svg-button" type="submit">
+                                    <div class="QBdPU B58H7">
+                                        <svg aria-label="Like" class="like-comment-hidden" color="#8e8e8e" fill="#8e8e8e" height="12" role="img" viewBox="0 0 48 48" width="12">
+                                            <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+                                        </svg>
+                                    </div>
+                                    <div class="QBdPU rrUvL">
+                                        <span class="">
+                                            <svg aria-label="Like" class="like-highlight-svg" color="#ed4956" fill="#ed4956" height="12" role="img" viewBox="0 0 48 48" width="12">
+                                                    <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </button>
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                                        <ul>
+                                                            @foreach ($errors->all() as $error)
+                                                                <li>{{ $error }}</li>
+                                                            @endforeach
+                                                        </ul>
+                                                    </div>
+                                @endif
+                            </form>
+                        @else
+                            <form class="like-highlight-form" method="POST" action="/highlights/s/{{$selected->slug}}/store-comment-like">
                             @csrf
                             <input type="hidden" name="highlight_id" id="highlight_id" value="{{$selected->id}}">
                             <input type="hidden" name="comment_id" id="comment_id" value="{{$comment->id}}">
@@ -91,15 +122,16 @@
                                 </div>
                             </button>
                             @if ($errors->any())
-                                <div class="alert alert-danger">
+                                    <div class="alert alert-danger">
                                                     <ul>
                                                         @foreach ($errors->all() as $error)
                                                             <li>{{ $error }}</li>
                                                         @endforeach
                                                     </ul>
                                                 </div>
-                            @endif
+                                @endif
                         </form>
+                        @endif
                     </div>
                 </span>
             </div>
--- resources/views/highlights/show.blade.php
+++ resources/views/highlights/show.blade.php
@@ -52,7 +52,13 @@
                             <div class="col highlight-selected-comment-column">
                                 <ul class="highlight-selected-comment-ul">
                                     @foreach($selected->comments as $comment)
-                                        <x-highlight-selected-comment :selected="$selected" :comment="$comment"/>
+                                        @if($hasCommentLikes->where('comment_id', '=', $comment->id)->count() > 0)
+                                        <x-highlight-selected-comment :selected="$selected" :comment="$comment"
+                                                                      :hasCommentLike="$hasCommentLikes->where('comment_id', '=', $comment->id)->first()->exists()"
+                                                                      :commentLike="$hasCommentLikes->where('comment_id', '=', $comment->id)->first()"/>
+                                        @else
+                                        <x-highlight-selected-comment :selected="$selected" :comment="$comment" :hasCommentLike="false"/>
+                                        @endif
                                     @endforeach
                                 </ul>
                             </div>
@@ -61,7 +67,11 @@
                         <div class="row selected-highlight-buttons-row">
                             <div class="col selected-highlight-buttons-col">
                                 <span>
-                                    <form class="like-highlight-form" method="POST" action="/highlights/s/{{$selected->slug}}/like">
+                                     @if($hasLike == false)
+                                    <form class="like-highlight-form" method="POST" action="/highlights/s/{{$selected->slug}}/store-like">
+                                    @else
+                                    <form class="like-highlight-form" method="POST" action="/highlights/s/{{$selected->slug}}/destroy-like/{{$like->id}}">
+                                    @endif
                                         @csrf
                                         <input type="hidden" name="highlight_id" id="highlight_id" value="{{$selected->id}}">
                                         <input type="hidden" name="user_id" id="user_id" value="{{auth()->user()->id}}">
@@ -72,9 +82,16 @@
                                             </svg>
                                         </div>
                                         <div class="like-highlight-real">
-                                            <svg aria-label="Like" class="like-highlight-svg" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 48 48" width="24">
+                                            @if($hasLike)
+                                                <svg aria-label="Like" class="like-highlight-svg" color="#ed4956" fill="#ed4956" height="24" role="img" viewBox="0 0 48 48" width="24">
+                                                    <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+                                                </svg>
+                                            @else
+                                                <svg aria-label="Like" class="like-highlight-svg" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 48 48" width="24">
                                                     <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
                                                 </svg>
+                                            @endif
+
                                         </div>
                                             @if ($errors->any())
                                                 <div class="alert alert-danger">
@@ -153,7 +170,7 @@
                         {{--Emoji and comment form section--}}
                         <div class="row comment-form-row">
                             <div class="col comment-form-column">
-                                <form class="comment-form" method="POST" action="/highlights/s/{{$selected->slug}}/comments">
+                                <form class="comment-form" method="POST" action="/highlights/s/{{$selected->slug}}/store-comments">
                                     @csrf
                                     <button class="emoji-button" type="button">
                                         <div>
